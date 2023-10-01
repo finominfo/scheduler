db38851e58a744e462c8e1edfc5ebbd939b63638
Kalman Kovacs <kalman.kovacs@gmail.com>
2023-10-01 12:50:06 +0200

    Organize imports

--- src/main/java/hu/finominfo/scheduler/Main.java
+++ src/main/java/hu/finominfo/scheduler/Main.java
@@ -1,22 +1,6 @@
 package hu.finominfo.scheduler;
 
-import hu.finominfo.scheduler.common.Globals;
-import hu.finominfo.scheduler.people.People;
-import hu.finominfo.scheduler.people.Type;
-import hu.finominfo.scheduler.scheduler.Scheduler;
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
