27b70a96bb5a66be9622a7f5168ca87b5f29e38d
Kalman Kovacs <kalman.kovacs@gmail.com>
2023-10-04 16:52:42 +0200

    small changes

--- src/main/java/hu/finominfo/scheduler/Main.java
+++ src/main/java/hu/finominfo/scheduler/Main.java
@@ -1,32 +1,19 @@
 package hu.finominfo.scheduler;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import hu.finominfo.scheduler.scheduler.Scheduler;
-import org.apache.logging.log4j.Level;
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.appender.ConsoleAppender;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.builder.api.AppenderComponentBuilder;
-import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilder;
-import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilderFactory;
-import org.apache.logging.log4j.core.config.builder.api.RootLoggerComponentBuilder;
-import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
+
 
 public class Main {
 
 
-    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+    private static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
 
 
     public static void main(String[] args) throws Exception {
-        //System.setProperty("log4j.configurationFile", String.valueOf(new File("resources", "log4j.xml").toURI()));
-        System.setProperty("log4j.configurationFile", "log4j2.properties");
-        System.setProperty("logFilename", "scheduler.log");
+        PropertyConfigurator.configure("log4j.properties");
+        LOGGER.warn("Started...");
         try {
             MainTask mainTask = new MainTask(args);
             mainTask.make();
