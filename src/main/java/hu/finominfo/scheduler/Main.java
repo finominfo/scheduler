1caa123c2c8019e7e4da3f7329397144c98af417
Kalman Kovacs <kalman.kovacs@gmail.com>
2023-10-04 20:33:38 +0200

    small change

--- src/main/java/hu/finominfo/scheduler/Main.java
+++ src/main/java/hu/finominfo/scheduler/Main.java
@@ -9,7 +9,6 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
-        logger.warn("Started...");
         try {
             MainTask mainTask = new MainTask(args);
             mainTask.make();
